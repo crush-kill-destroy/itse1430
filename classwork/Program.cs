13c98136f544b1da3bf0fd6492e8ae7e92a7d784
Me <Me@DESKTOP-S3VGLRP>
2020-08-26 21:34:38 -0500

    This

---
+++ classwork/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int hours = 10;
+            //
+            int pay = hours * 9;
+        }
+    }
+}
